d86ff098fe8336acb5e178b29b39e1490ecef7f6
new name <chen.ky@northeastern.edu>
2022-10-23 14:14:56 -0400

    a6 React.js f22

--- src/tuiter/who-to-follow-list/who-to-follow-list-item.js
+++ src/tuiter/who-to-follow-list/who-to-follow-list-item.js
@@ -5,7 +5,7 @@ const WhoToFollowListItem = ({who}
         <li className="list-group-item">
             <div className="row">
                 <div className="col-2">
-                    <img className="rounded-circle" height={48} src={who.avatarIcon}/>
+                    <img className="rounded-circle" alt={who.alt} height={48} src={who.avatarIcon}/>
                 </div>
                 <div className="col-8">
                     <div className="fw-bold">{who.userName}</div>
